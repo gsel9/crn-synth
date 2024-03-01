f34eb0008a4f07ed2a21dac6cd8c93edd7a169e1
daanknoors <d.knoors@gmail.com>
2024-03-01 12:55:39 +0100

    set global random state

--- crnsynth2/generators/base_generator.py
+++ crnsynth2/generators/base_generator.py
@@ -1,24 +1,40 @@
 import pickle
+import random
 from pathlib import Path
 from typing import Any, Union
 
+import numpy as np
 import pandas as pd
 
 
 class BaseGenerator:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, random_state: Union[int, None] = None):
+        self.random_state = random_state
+
+        # set global random seed for both numpy and random
+        if random_state is not None:
+            np.random.seed(random_state)
+            random.seed(random_state)
 
     @property
     def name(self) -> str:
-        """Return the name of the generator."""
-        raise NotImplementedError("name() method not implemented")
+        return self.__class__.__name__
 
-    def fit(self, data_real, **kwargs: Any) -> None:
+    def fit(self, data_real) -> None:
         raise NotImplementedError("fit() method not implemented")
 
     def generate(self, n_records: int) -> pd.DataFrame:
         raise NotImplementedError("generate() method not implemented")
+
+    def set_params(self, **params):
+        """Set parameters"""
+        for key, value in params.items():
+            # check if attribute exists
+            if hasattr(self, key):
+                setattr(self, key, value)
+
+    def get_params(self):
+        return self.__dict__
 
     def save(self, path: Union[str, Path]) -> None:
         with open(path, "wb") as f:
@@ -28,3 +44,9 @@ class BaseGenerator:
     def load(path: Union[str, Path]) -> Any:
         with open(path, "rb") as f:
             return pickle.load(f)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+
+    def __str__(self) -> str:
+        return self.__repr__()
