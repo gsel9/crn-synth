2641edc20cbb329fcb6a3e34eda4f480038fbd3f
daanknoors <d.knoors@gmail.com>
2024-04-23 12:25:25 +0200

    add record sampling option to dummy generator

--- crnsynth/generators/dummy.py
+++ crnsynth/generators/dummy.py
@@ -2,12 +2,24 @@ from crnsynth.generators.base import BaseGenerator
 
 
 class DummyGenerator(BaseGenerator):
-    """Makes a copy of the original dataset. Useful as a baseline for privacy metrics.
+    """Samples records from the original dataset. Useful as a baseline for privacy metrics.
 
-    CAUTION: don't use this for generating synthetic data based on sensitive data!"""
+    When setting sample_with_replace to False and n_records equal to the original dataset,
+    the generator will just create a copy of the fitted data.
 
-    def __init__(self, **kwargs):
+    CAUTION: do not release the output of this generator when based on sensitive data!
+    """
+
+    def __init__(self, sample_with_replace=False, random_state=None, **kwargs):
+        """Initialize the generator.
+
+        Args:
+            sample_with_replace (bool): if true record from data_real can be sampled more than once. Default is False.
+            random_state (int): random state for reproducibility. Default is None.
+        """
         super().__init__()
+        self.sample_with_replace = sample_with_replace
+        self.random_state = random_state
         self.model = None
 
     def fit(self, data_real) -> None:
@@ -16,8 +28,25 @@ class DummyGenerator(BaseGenerator):
 
     def generate(self, n_records: int):
         """Copy the real data"""
-        if n_records != len(self.model):
+        if (self.sample_with_replace is False) and (n_records > len(self.model)):
             raise ValueError(
-                "n_records should be equal to the number of records in the real data."
+                "Cannot sample more records than the original dataset. "
+                "Reduce n_records or set sample_with_replace=True."
             )
-        return self.model.copy()
+
+        # copy original dataframe when sample_with_replace is False and n_records is equal to the original dataset
+        if (self.sample_with_replace is False) and (n_records == len(self.model)):
+            data_synth = self.model.copy()
+
+        # sample records from the original dataset with or without replacement
+        else:
+            data_synth = (
+                self.model.sample(
+                    n_records,
+                    replace=self.sample_with_replace,
+                    random_state=self.random_state,
+                )
+                .copy()
+                .reset_index(drop=True)
+            )
+        return data_synth
--- tests/generators/test_dummy.py
+++ tests/generators/test_dummy.py
@@ -16,14 +16,37 @@ def data_real():
     )
 
 
-def test_dummy_generator(data_real):
-    generator = DummyGenerator(epsilon=0.1)
+def test_dummy_generator_copy_data(data_real):
+    """Test that the DummyGenerator with sample_with_replace == False and all records see if it copies the data."""
+    generator = DummyGenerator(sample_with_replace=False)
     generator.fit(data_real)
-    data_synth = generator.generate(4)
-    assert data_synth.shape == data_real.shape
-    assert data_synth.columns.tolist() == data_real.columns.tolist()
-    assert data_synth.dtypes.tolist() == data_real.dtypes.tolist()
+    data_synth = generator.generate(data_real.shape[0])
+    assert data_real.equals(data_synth)
+
+
+def test_dummy_generator_with_replace(data_real):
+    """Test that the DummyGenerator with sample_with_replace == True and all records see if it does not copy the data."""
+    generator = DummyGenerator(sample_with_replace=True, random_state=42)
+    generator.fit(data_real)
+    data_synth = generator.generate(data_real.shape[0])
+    assert not data_real.equals(data_synth)
+
+
+def test_dummy_generator_without_replace_too_many_rcords(data_real):
+    """Test that the DummyGenerator with sample_with_replace == False and more records than the original dataset raises an error."""
+    generator = DummyGenerator(sample_with_replace=False)
+    generator.fit(data_real)
+    with pytest.raises(ValueError):
+        generator.generate(data_real.shape[0] + 1)
+
+
+def test_dummy_generator_without_replace(data_real):
+    """Test that the DummyGenerator with sample_with_replace == False and less records than the original dataset copies the data."""
+    generator = DummyGenerator(sample_with_replace=False)
+    generator.fit(data_real)
+    data_synth = generator.generate(data_real.shape[0] - 1)
+    assert data_synth.shape[0] == data_real.shape[0] - 1
+    assert data_real.columns.tolist() == data_synth.columns.tolist()
+    assert data_real.dtypes.tolist() == data_synth.dtypes.tolist()
     assert data_synth.isnull().sum().sum() == 0
     assert data_synth.notna().all().all()
-
-    assert data_real.equals(data_synth)
