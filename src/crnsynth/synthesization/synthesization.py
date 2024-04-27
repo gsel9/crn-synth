f66904b43365bbc5b9bca0e77d8bd6f5ce750a85
Severin Langberg <langberg91@gmail.com>
2024-04-27 15:51:39 +0200

    tweaks to make min example

--- src/crnsynth/synthesization/synthesization.py
+++ src/crnsynth/synthesization/synthesization.py
@@ -1,7 +1,7 @@
 """Functions to run synthesis pipeline on real data."""
 
 from pathlib import Path
-from typing import Union
+from typing import Union, Tuple, Optional
 
 import pandas as pd
 
@@ -12,22 +12,34 @@ from crnsynth.serialization.paths import create_output_dir
 def generate_synth_data(
     data_real: pd.DataFrame,
     generator: BaseGenerator,
-    n_records: Union[None, int],
-    verbose: int = 1,
-) -> tuple[pd.DataFrame, BaseGenerator]:
-    """Generate synthetic data using a generator."""
-    if n_records is None:
-        n_records = data_real.shape[0]
+    n_samples: Optional[int] = None,
+    verbose: bool = True 
+) -> Tuple[pd.DataFrame, BaseGenerator]:
+    """ Generate synthetic data using a generator.
 
-    # fit generator
+    Args:
+        data_real (pd.DataFrame): _description_
+        generator (BaseGenerator): _description_
+        n_samples (Optional[int], optional): _description_. Defaults to None.
+        verbose (bool, optional): _description_. Defaults to True.
+
+    Returns:
+        Tuple[pd.DataFrame, BaseGenerator]: _description_
+    """
+    if n_samples is None:
+        n_samples = data_real.shape[0]
+
     if verbose:
         print(f"Fitting generator {generator} on input data")
+    
+    # fit generator
     generator.fit(data_real)
 
-    # generate synthetic data
     if verbose:
-        print(f"Generator fitted. Generating {n_records} records")
-    data_synth = generator.generate(n_records)
+        print(f"Generating {n_samples} data samples")
+        
+    # generate synthetic data
+    data_synth = generator.generate(n_samples)
 
     # return synthetic data and fitted generator
     return data_synth, generator
