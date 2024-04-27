31feda990252ef82880851404d824729fac4c652
Severin Langberg <langberg91@gmail.com>
2024-04-27 14:43:44 +0200

    reorg to get install working

--- setup.py
+++ setup.py
@@ -1,18 +1,44 @@
-from setuptools import find_packages, setup
+# stdlib
+import os
 
-setup(
-    name="crnsynth",
-    version="0.1.0",
-    description="Synthetic data generation methods for the Cancer Registry of Norway",
-    author="Daan Knoors, Severin Elvatun",
-    author_email="dk@daanknoors.com, sela@kreftregisteret.no",
-    packages=find_packages(),
-    install_requires=[
-        "synthetic-data-generation>=0.1.14",
-        "synthcity",
-        "diffprivlib>=0.6.3",
-        "sdmetrics",
-        "gower",
-    ],
-    extras_require={"interactive": ["matplotlib", "jupyter"]},
-)
+# third party
+from setuptools import setup
+
+PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def read(fname: str) -> str:
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def get_version() -> str:
+    return "1.0.0"
+
+
+if __name__ == "__main__":
+    try:
+        setup(
+            name="crnsynth",
+            version=get_version(),
+            description="Synthetic data generation methods for the Cancer Registry of Norway",
+            author="Daan Knoors, Severin Elvatun",
+            author_email="dk@daanknoors.com, sela@kreftregisteret.no",
+            long_description=read("README.md"),
+            install_requires=[
+                "synthetic-data-generation>=0.1.14",
+                "synthcity",
+                "diffprivlib>=0.6.3",
+                "sdmetrics",
+                "gower",
+            ],
+            extras_require={"interactive": ["matplotlib", "jupyter"]},
+        )
+    except:  
+        # error 
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
